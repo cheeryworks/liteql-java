38c2f6f1e68cea8feb6cf31ee8ce113e10a4c97d
Carr Chang <carr.z.chang@outlook.com>
2020-06-11 16:45:50 +0800

    Add multipath support to FileSystemRepository

--- liteql-service/src/main/java/org/cheeryworks/liteql/service/repository/FileSystemRepository.java
+++ liteql-service/src/main/java/org/cheeryworks/liteql/service/repository/FileSystemRepository.java
@@ -2,7 +2,7 @@ package org.cheeryworks.liteql.service.repository;
 
 import org.cheeryworks.liteql.model.type.DomainType;
 import org.cheeryworks.liteql.model.type.migration.Migration;
-import org.cheeryworks.liteql.model.util.json.JsonReader;
+import org.cheeryworks.liteql.model.util.FileReader;
 import org.cheeryworks.liteql.model.util.json.LiteQLJsonUtil;
 
 import java.util.Arrays;
@@ -15,46 +15,48 @@ import java.util.TreeMap;
 
 public class FileSystemRepository implements Repository {
 
-    private String path;
+    private String[] paths;
 
     private Map<String, Map<String, DomainType>> schemas = new LinkedHashMap<>();
 
     private Map<String, Map<String, Migration>> migrations = new LinkedHashMap<>();
 
-    public FileSystemRepository(String path) {
-        this.path = path;
+    public FileSystemRepository(String... paths) {
+        this.paths = paths;
 
         init();
     }
 
     private void init() {
-        Map<String, String> typeJsonFiles = JsonReader.readJsonFiles(
-                path + (path.endsWith("/") ? "" : "/") + NAME_OF_TYPES_DIRECTORY);
+        for (String path : paths) {
+            Map<String, String> typeJsonFiles = FileReader.readJsonFilesRecursively(
+                    path + (path.endsWith("/") ? "" : "/") + NAME_OF_TYPES_DIRECTORY);
 
-        for (Map.Entry<String, String> typeJsonFileEntry : typeJsonFiles.entrySet()) {
-            String typePath = typeJsonFileEntry.getKey();
-            String[] definitionInformation = typePath.split("/");
-            String schema = definitionInformation[0];
-            String domainTypeName = definitionInformation[0] + "." + definitionInformation[1];
+            for (Map.Entry<String, String> typeJsonFileEntry : typeJsonFiles.entrySet()) {
+                String typePath = typeJsonFileEntry.getKey();
+                String[] definitionInformation = typePath.split("/");
+                String schema = definitionInformation[0];
+                String domainTypeName = definitionInformation[0] + "." + definitionInformation[1];
 
-            if (typePath.endsWith(NAME_OF_TYPE_DEFINITION)) {
-                DomainType domainType = LiteQLJsonUtil.toBean(typeJsonFileEntry.getValue(), DomainType.class);
+                if (typePath.endsWith(NAME_OF_TYPE_DEFINITION)) {
+                    DomainType domainType = LiteQLJsonUtil.toBean(typeJsonFileEntry.getValue(), DomainType.class);
 
-                domainType.setSchema(schema);
-                domainType.setName(domainTypeName);
+                    domainType.setSchema(schema);
+                    domainType.setName(domainTypeName);
 
-                addType(schema, domainTypeName, domainType);
-            }
+                    addType(schema, domainTypeName, domainType);
+                }
 
-            if (typePath.contains("/" + NAME_OF_MIGRATIONS_DIRECTORY + "/")) {
-                String migrationName = definitionInformation[3].substring(
-                        0, definitionInformation[3].indexOf(SUFFIX_OF_CONFIGURATION_FILE));
-                Migration migration = LiteQLJsonUtil.toBean(typeJsonFileEntry.getValue(), Migration.class);
-                migration.setName(migrationName);
-                migration.setSchema(schema);
-                migration.setDomainType(domainTypeName);
+                if (typePath.contains("/" + NAME_OF_MIGRATIONS_DIRECTORY + "/")) {
+                    String migrationName = definitionInformation[3].substring(
+                            0, definitionInformation[3].indexOf(SUFFIX_OF_CONFIGURATION_FILE));
+                    Migration migration = LiteQLJsonUtil.toBean(typeJsonFileEntry.getValue(), Migration.class);
+                    migration.setName(migrationName);
+                    migration.setSchema(schema);
+                    migration.setDomainType(domainTypeName);
 
-                addMigration(schema, migrationName, migration);
+                    addMigration(schema, migrationName, migration);
+                }
             }
         }
     }
