39abd74e6402ca4b24eae6c92e896e5e767db128
Carr Chang <carr.z.chang@outlook.com>
2020-07-04 13:58:36 +0800

    Clean Jackson ObjectMapper configuration

--- liteql-model/src/main/java/org/cheeryworks/liteql/model/util/LiteQLJsonUtil.java
+++ liteql-model/src/main/java/org/cheeryworks/liteql/model/util/LiteQLJsonUtil.java
@@ -92,7 +92,7 @@ public final class LiteQLJsonUtil {
         builder.featuresToDisable(
                 SerializationFeature.WRITE_DATES_AS_TIMESTAMPS,
                 DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-        builder.serializationInclusion(JsonInclude.Include.NON_EMPTY);
+        builder.serializationInclusion(JsonInclude.Include.NON_NULL);
 
         builder.deserializerByType(ConditionClause.class, new ConditionClauseDeserializer());
         builder.deserializerByType(ConditionOperator.class, new ConditionOperatorDeserializer());
