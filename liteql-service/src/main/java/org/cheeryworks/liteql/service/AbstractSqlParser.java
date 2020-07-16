9567466fbf39b07359eefd4fab244fe038c049a5
Carr Chang <carr.z.chang@outlook.com>
2020-07-16 21:52:04 +0800

    Clean up AbstractSqlParser

--- liteql-service/src/main/java/org/cheeryworks/liteql/service/AbstractSqlParser.java
+++ liteql-service/src/main/java/org/cheeryworks/liteql/service/AbstractSqlParser.java
@@ -1,5 +1,5 @@
 package org.cheeryworks.liteql.service;
 
-public class AbstractSqlParser implements SqlParser, SqlCustomizer {
+public abstract class AbstractSqlParser implements SqlParser, SqlCustomizer {
 
 }
