0ac08fa226856a85dc04718989a66b4ef8345d63
Carr Chang <carr.z.chang@outlook.com>
2020-08-04 17:36:34 +0800

    Clean up HierarchicalEntity

--- liteql-model/src/main/java/org/cheeryworks/liteql/model/HierarchicalEntity.java
+++ liteql-model/src/main/java/org/cheeryworks/liteql/model/HierarchicalEntity.java
@@ -4,7 +4,7 @@ import org.cheeryworks.liteql.schema.annotation.Position;
 import org.cheeryworks.liteql.schema.annotation.Type;
 
 @Type
-public interface HierarchicalEntity<T> extends SortableEntity {
+public interface HierarchicalEntity extends SortableEntity {
 
     @Position(1)
     String getParentId();
