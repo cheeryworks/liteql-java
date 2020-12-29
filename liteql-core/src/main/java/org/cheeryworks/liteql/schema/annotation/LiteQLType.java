30b65d21552e8cb4889444dd15a136a137ffd045
Carr Chang <carr.z.chang@outlook.com>
2020-12-29 12:24:50 +0800

    Clean up annotations

--- liteql-core/src/main/java/org/cheeryworks/liteql/schema/annotation/LiteQLType.java
+++ liteql-core/src/main/java/org/cheeryworks/liteql/schema/annotation/LiteQLType.java
@@ -9,8 +9,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface LiteQLType {
 
-    boolean ignored() default false;
-
     LiteQLTypeIndex[] indexes() default {};
 
 }
