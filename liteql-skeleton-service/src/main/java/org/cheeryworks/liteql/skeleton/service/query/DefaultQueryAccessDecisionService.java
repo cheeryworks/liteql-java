8e59a738e6fd525099d464d8a6f3b004a87f7e2d
Carr Chang <carr.z.chang@outlook.com>
2021-01-17 10:58:06 +0800

    Clean up DefaultQueryAccessDecisionService

--- liteql-skeleton-service/src/main/java/org/cheeryworks/liteql/skeleton/service/query/DefaultQueryAccessDecisionService.java
+++ liteql-skeleton-service/src/main/java/org/cheeryworks/liteql/skeleton/service/query/DefaultQueryAccessDecisionService.java
@@ -2,7 +2,6 @@ package org.cheeryworks.liteql.skeleton.service.query;
 
 import org.cheeryworks.liteql.skeleton.model.UserType;
 import org.cheeryworks.liteql.skeleton.query.DomainQuery;
-import org.cheeryworks.liteql.skeleton.service.query.QueryAccessDecisionService;
 
 public class DefaultQueryAccessDecisionService implements QueryAccessDecisionService {
 
