2812d5f8003b5a693403bf7421ce83a925730200
Carr Chang <carr.z.chang@outlook.com>
2023-06-17 22:54:29 +0800

    Update driver and validation query of HSQLDatabase

--- liteql-skeleton-service/src/main/java/org/cheeryworks/liteql/skeleton/database/HSQLDatabase.java
+++ liteql-skeleton-service/src/main/java/org/cheeryworks/liteql/skeleton/database/HSQLDatabase.java
@@ -4,7 +4,12 @@ public class HSQLDatabase extends AbstractDatabaseType {
 
     @Override
     public final String getClassName() {
-        return "org.hsqldb.jdbcDriver";
+        return "org.hsqldb.jdbc.JDBCDriver";
+    }
+
+    @Override
+    public final String getValidationQuery() {
+        return "select 1 from INFORMATION_SCHEMA.SYSTEM_USERS";
     }
 
 }
